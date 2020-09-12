087834516ca39c811b3675492a329c24ba53e41b
rsaarelm <risto.saarelma@iki.fi>
2020-09-12 18:29:24 +0300

    WIP CLI

--- src/aoc/main.clj
+++ src/aoc/main.clj
@@ -1,4 +1,15 @@
-(ns aoc2015.main)
+(ns aoc.main
+  (:require [cli-matic]))
+
+(defn- run []
+  (print "TODO"))
+
+(def CLI
+  {:command     "aoc"
+   :description "Advent of Code runner"
+   :subcommands [{:command  "run"
+                  :description "Run code samples"
+                  :runs run}]})
 
 (defn -main []
   (print "MAIN!"))
