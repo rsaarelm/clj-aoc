1ee940d83380591301ddda452aeaef4f20069146
rsaarelm <risto.saarelma@iki.fi>
2020-09-12 13:26:43 +0300

    Tighten

--- src/aoc/util.clj
+++ src/aoc/util.clj
@@ -1,18 +1,6 @@
 (ns aoc.util
   (:require [clojure.string :as str]))
 
-(defn- my-split-lines
-  "Makes sure lines ending with \\n\\n get an empty line at the end."
-  [text]
-  (let [lines (str/split-lines text)]
-    (cond
-      (str/ends-with? text "\n\n") (conj lines "")
-      :else lines)))
-
-(defn- pop-if
-  [p s]
-  (if (p (peek s)) (pop s) s))
-
 (defn unfortune
   "Convert text in unix fortune format into separated text chunks.
 
@@ -21,18 +9,18 @@
   The last newline is omitted to allow newline-less single line inputs. To add
   a newline, leave an empty line before the separating '%'.
 
-  A '%' after the last item is optional. This will show up in the parser as
-  the last element of the sequence being an empty string, which will then be
-  ignored.
+  The last item can be followed by an optional terminal '%'.
 
   BUGS
   - Any number of trailing newlines will be collapsed to a single newline at
-    end.
-  "
+    end."
   [text]
   (->> text
-       (my-split-lines)
-       (pop-if #{"%"})
+       (str/split-lines)
+       ; Hack around str/split-lines eating trailing newlines.
+       (#(if (str/ends-with? text "\n\n") (conj % "") %))
+       ; Ignore terminal '%'.
+       (#(if (= (peek %) "%") (pop %) %))
        (reduce
          (fn [acc line]
            (if (= line "%")
