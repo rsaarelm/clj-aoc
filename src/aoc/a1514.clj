b6fe8a2f33bbd77a0416646e3c3444c8443bd4a1
rsaarelm <risto.saarelma@iki.fi>
2020-09-19 08:13:38 +0300

    Day 2015.14

---
+++ src/aoc/a1514.clj
@@ -0,0 +1,10 @@
+(ns aoc.a1514
+  ;(:require [aoc.util :refer [re-read]])
+  ;(:require [clojure.string :as str])
+  )
+
+(defn- parse [input] input)
+
+(defn p1 [input] (parse input))
+
+(defn p2 [input] (parse input))
